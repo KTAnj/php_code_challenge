db3878793b092273c7539a6286ea183d32f2f606
KTAnj <thilininiroshi@gmail.com>
2021-01-13 23:03:04 +0530

    Revert "Write feature test for sample API"

--- back-end/app/Repositories/PlayerRepositoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Repositories;
-
-interface PlayerRepositoryInterface {
-    public function find($id);
-}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -15,4 +15,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('/bet', 'BetController');
-Route::get('/player/{id}', 'BetController@getPlayer');
